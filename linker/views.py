efe8a4bfbbffb287ba14557cb493406448255077
Tiago Bortoletto Vaz <tiago@debian.org>
2016-05-10 18:13:25 -0400

    Listing broken links only.

--- linker/views.py
+++ linker/views.py
@@ -6,11 +6,12 @@ from models import PDF
 
 class List(View):
     def get(self, request):
-        data = PDF.objects.all()
+        data = PDF.objects.filter(found__contains='0')
+        other_data = data.count()
         data_table = PDFTable(data)
         data_table.paginate(page=request.GET.get('page', 1), per_page=25)
         RequestConfig(request).configure(data_table)
-        return render(request, 'list.html', {'table': data_table})
+        return render(request, 'list.html', {'table': data_table, 'other_data' : other_data})
 
     def post(self, request):
         pass
