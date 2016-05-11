1a5d3e93c52a4bc9c25d32a0eb25ef9d7e2912c2
Tiago Vaz <tiago@acaia.ca>
2016-05-11 12:41:32 +0000

    removing media/doc...

--- linker/forms.py
+++ linker/forms.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django import forms
 from models import PDF
 
 class DocumentForm(forms.ModelForm):
-    docfile = forms.FileField(label='Select a file')
+    docfile = forms.FileField(label='Sélectionnez le fichier PDF')
 
     class Meta:
         model = PDF
